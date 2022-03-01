5b5247c51bd76fc8543544358d30a1144c3b0529
kmoussa-dev <konemoussamohamed@gmail.com>
2022-03-01 21:05:50 +0100

    Ajout des entités et repository

--- src/main/java/fr/univorleans/mongodb/MongoDbApplication.java
+++ src/main/java/fr/univorleans/mongodb/MongoDbApplication.java
@@ -2,8 +2,10 @@ package fr.univorleans.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories("fr.univ-orleans.mongodb.repository")
 public class MongoDbApplication {
 
     public static void main(String[] args) {
