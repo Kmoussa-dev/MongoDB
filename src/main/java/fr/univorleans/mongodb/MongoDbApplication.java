759e834477ded04eeb6f8b141ce7bc325d9c2dab
kmoussa-dev <konemoussamohamed@gmail.com>
2022-03-01 21:06:46 +0100

    correction Repository

--- src/main/java/fr/univorleans/mongodb/MongoDbApplication.java
+++ src/main/java/fr/univorleans/mongodb/MongoDbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories("fr.univ-orleans.mongodb.repository")
+@EnableMongoRepositories("fr.univorleans.mongodb.repository")
 public class MongoDbApplication {
 
     public static void main(String[] args) {
